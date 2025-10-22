5ca7a22cb2b938da63a0d3462dac08ce12efe86d
Andy <babyandy0111@gmail.com>
2025-10-23 01:12:31 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/chill-demo/',
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  if (command === 'build') {
+    return {
+      base: '/chill-demo/', // For GitHub Pages deployment
+      plugins: [react()],
+    }
+  } else {
+    return {
+      base: '/', // For local development
+      plugins: [react()],
+    }
+  }
 })
